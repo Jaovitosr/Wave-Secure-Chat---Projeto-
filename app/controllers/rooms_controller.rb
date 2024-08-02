2b7988680629657cad7f64e089a2118f55f2c3ef
Jaovitosr <jv.souzarocha@gmail.com>
2024-08-02 02:22:41 -0300

    Implementacao inicial

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,28 @@
+class RoomsController < ApplicationController
+
+  def index
+    @current_user = current_user
+    unless @current_user
+      redirect_to '/signin' and return
+    end
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+  end
+
+  def create
+    @room = Room.create(name: params["room"] ["name"])
+  end
+
+  def show
+    @current_user = current_user
+    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room = Room.new
+    @message = Message.new
+
+    render "index"
+  end
+end
+
+  
---
+++ app/models/room.rb
@@ -0,0 +1,18 @@
+class CreateRooms < ActiveRecord::Migration[6.1]
+    def change
+      create_table :rooms do |t|
+      t.string :name
+      t.boolean :is_private, :default => false
+  
+      t.timestamps
+  
+      end
+    end
+  end
+
+  class Room < ApplicationRecord
+    validates_uniqueness_of :name
+    scope :public_rooms, -> { where(is_private: false) }
+    after_create_commit {broadcast_append_to "rooms"}
+    has_many :messages
+  end
